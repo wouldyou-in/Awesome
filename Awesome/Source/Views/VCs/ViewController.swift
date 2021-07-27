a633547e150bd4b24f860a00f47618a7445e3514
parkikbum <pibum@naver.com>
2021-07-27 21:49:28 +0900

    [ADD] 어썸 리팩토링 프로젝트 생성

---
+++ Awesome/Source/Views/VCs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Awesome
+//
+//  Created by 박익범 on 2021/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Awesome' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+pod 'FSCalendar'
+pod 'Alamofire', '~> 5.2'
+  # Pods for Awesome
+
+end
