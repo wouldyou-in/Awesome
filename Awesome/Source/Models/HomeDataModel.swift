d5fc7df1d19ef2adabda553bb69e95aed6e79dd5
parkikbum <pibum@naver.com>
2021-07-28 03:18:14 +0900

    [ADD]홈 데이터 모델 추가

--- Awesome/Source/Models/HomeDataModel.swift
+++ Awesome/Source/Models/HomeDataModel.swift
@@ -1,8 +1,33 @@
-//
-//  HomeDataModel.swift
-//  Awesome
-//
-//  Created by 박익범 on 2021/07/28.
-//
 
 import Foundation
+
+// MARK: - CalendarDataModel
+struct CalendarDataModel: Codable {
+    let createdCalendar, myCalendar, calendar: [MyCalendar]
+
+    enum CodingKeys: String, CodingKey {
+        case createdCalendar = "created_calendar"
+        case myCalendar = "my_calendar"
+        case calendar
+    }
+}
+
+// MARK: - Calendar
+struct MyCalendar: Codable {
+    let creatorName: String
+    let participantName: String
+    let id, creator, participant: Int
+    let comment: String
+    let startDate, endDate: Date
+    let createdAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, creator, participant, comment
+        case creatorName = "creator_name"
+        case participantName = "participant_name"
+        case startDate = "start_date"
+        case endDate = "end_date"
+        case createdAt = "created_at"
+    }
+}
+
