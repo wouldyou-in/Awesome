73bf707db8d7032affd62a727941a969e1bf25f4
parkikbum <pibum@naver.com>
2021-07-28 03:19:32 +0900

    [ADD] 익스텐션 추가

--- Awesome/Source/Extension/Extension+TableView.swift
+++ Awesome/Source/Extension/Extension+TableView.swift
@@ -1,8 +1,18 @@
-//
-//  Extension+TableView.swift
-//  Awesome
-//
-//  Created by 박익범 on 2021/07/28.
-//
 
-import Foundation
+import UIKit
+
+extension UITableView {
+    
+    public func registerCustomXib(xibName: String){
+        let xib = UINib(nibName: xibName, bundle: nil)
+        self.register(xib, forCellReuseIdentifier: xibName)
+    }
+    
+//    func dequeueReusableCell<T: UITableViewCell>(for indexPath: IndexPath) -> T {
+//        guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+//            fatalError("Unable Dequeue Reusable")
+//        }
+//        return cell
+//    }
+    
+}
--- Awesome/Source/Extension/Extension+UIFont.swift
+++ Awesome/Source/Extension/Extension+UIFont.swift
@@ -1,8 +1,22 @@
-//
-//  Extension+UIFont.swift
-//  Awesome
-//
-//  Created by 박익범 on 2021/07/27.
-//
+import UIKit
 
-import Foundation
+struct AppFontName {
+    static let bold = "GmarketSansTTFBold"
+    static let light = "GmarketSansTTFLight"
+    static let medium = "GmarketSansTTFMedium"
+}
+
+extension UIFont{
+    
+    @objc class func gmarketSansBoldFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.bold, size: size)!
+    }
+
+    @objc class func gmarketSansLightFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.light, size: size)!
+    }
+    
+    @objc class func gmarketSansMediumFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.medium, size: size)!
+    }
+}
