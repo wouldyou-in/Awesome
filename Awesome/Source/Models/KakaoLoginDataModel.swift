9ce65acfd560935fefc91a9818928b49c74c59ef
parkikbum <pibum@naver.com>
2021-07-28 22:23:31 +0900

    [CHORE] 시작 화면 변경

---
+++ Awesome/Source/Models/KakaoLoginDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  KakaoLoginDataModel.swift
+//  Awesome
+//
+//  Created by 박익범 on 2021/07/28.
+//
+
+import Foundation
---
+++ Awesome/Source/Services/KakaoLoginService.swift
@@ -0,0 +1,8 @@
+//
+//  KakaoLoginService.swift
+//  Awesome
+//
+//  Created by 박익범 on 2021/07/28.
+//
+
+import Foundation
