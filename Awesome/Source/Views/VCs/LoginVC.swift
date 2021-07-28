873e99cca36f6e82a6ab8c93cfa2d1ba66b3debd
parkikbum <pibum@naver.com>
2021-07-28 22:24:22 +0900

    [ADD] 초기 로그인 화면

--- Awesome/Source/Views/VCs/LoginVC.swift
+++ Awesome/Source/Views/VCs/LoginVC.swift
@@ -12,18 +12,14 @@ class LoginVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//MARK: IBAction
+    @IBAction func kakaoLoginButtonClicked(_ sender: Any) {
+        guard let kakaoVC = UIStoryboard(name: "KakaoLogin", bundle: nil).instantiateViewController(identifier: "KakaoLoginVC") as? KakaoLoginVC else {return}
+        self.navigationController?.pushViewController(kakaoVC, animated: true)
     }
-    */
-
+    @IBAction func appleLoginButtonClicked(_ sender: Any) {
+    }
+    
 }
