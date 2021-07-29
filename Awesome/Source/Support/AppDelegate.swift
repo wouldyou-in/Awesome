27dd58fd68cf91961583419008e2f387554b2f79
parkikbum <pibum@naver.com>
2021-07-29 16:54:29 +0900

    [CHORE] 스플래시 구현

--- Awesome/Source/Support/AppDelegate.swift
+++ Awesome/Source/Support/AppDelegate.swift
@@ -13,13 +13,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if UserDefaults.standard.bool(forKey: "loginBool") == true{
+            SplashVC.getUpdateToken()
+            sleep(1)
+        }
+        else {
+            sleep(1)
+        }
+            
+
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
