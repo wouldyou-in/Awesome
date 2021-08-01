b0ec86b02e7e70bba9e94a4ab63af30ab82a14bd
parkikbum <pibum@naver.com>
2021-08-01 23:35:25 +0900

    [ADD] date피커뷰

--- Awesome/Source/Views/VCs/NoScheduleDatePickerVC.swift
+++ Awesome/Source/Views/VCs/NoScheduleDatePickerVC.swift
@@ -8,22 +8,43 @@
 import UIKit
 
 class NoScheduleDatePickerVC: UIViewController {
+//MARK: IBOulet
+    @IBOutlet weak var pickerView: UIDatePicker!
+    @IBOutlet weak var okButton: UIButton!
+    
+//MARK: var
+    var chooseDate : String = ""
+    var startDelegate : dateData?
+    var endDelegate: dateData?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setLayout()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//MARK: func
+    func setLayout(){
+        okButton.backgroundColor = UIColor.mainPink
+        okButton.clipsToBounds = true
+        okButton.layer.cornerRadius = 15
     }
-    */
+
+    func changed(){
+           let dateformatter = DateFormatter()
+        dateformatter.dateStyle = .short
+           dateformatter.timeStyle = .none
+        chooseDate = dateformatter.string(from: pickerView.date)
+    }
+    
+//MARK: IBAction
+    @IBAction func datepickerSelected(_ sender: Any) {
+        changed()
+    }
+    @IBAction func okButtonClicked(_ sender: Any) {
+        startDelegate?.startDataSend(data: chooseDate)
+        endDelegate?.finishDateSend(data: chooseDate)
+        self.dismiss(animated: true, completion: nil)
+    }
 
 }
