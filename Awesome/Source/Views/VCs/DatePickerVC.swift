96d7b3bfed3f1b8ae77232e797f53bb0cac3f137
parkikbum <pibum@naver.com>
2021-08-01 17:25:46 +0900

    [ADD]데이트 피커 구현

--- Awesome/Source/Views/VCs/DatePickerVC.swift
+++ Awesome/Source/Views/VCs/DatePickerVC.swift
@@ -7,23 +7,49 @@
 
 import UIKit
 
-class DatePickerVC: UIViewController {
+protocol dateData {
+    func startDataSend(data : String)
+    func finishDateSend(data : String)
+}
 
+
+class DatePickerVC: UIViewController {
+    
+//MARK: viewDidLoad
+    @IBOutlet weak var pickerView: UIDatePicker!
+    var chooseDate: String = ""
+    var startDelegate: dateData?
+    var endDelegate: dateData?
+    @IBOutlet weak var okButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setLayout()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func datepickerSelected(_ sender: Any) {
+        changed()
     }
-    */
-
+    func setLayout(){
+        okButton.backgroundColor = UIColor.mainPink
+        okButton.clipsToBounds = true
+        okButton.layer.cornerRadius = 15
+    }
+    
+    func changed(){
+           let dateformatter = DateFormatter()
+           dateformatter.dateStyle = .none
+        dateformatter.timeStyle = .short
+        dateformatter.locale = Locale(identifier: "en_GB")
+        print(pickerView.date)
+        chooseDate = dateformatter.string(from: pickerView.date)
+           
+    }
+    @IBAction func okButtonClicked(_ sender: Any) {
+        startDelegate?.startDataSend(data: chooseDate)
+        endDelegate?.finishDateSend(data: chooseDate)
+        self.dismiss(animated: true, completion: nil)
+        print(chooseDate)
+    }
 }
