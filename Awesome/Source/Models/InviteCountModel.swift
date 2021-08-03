6b9270c8d999b6f7328dccb3ee2d609ca043c514
parkikbum <pibum@naver.com>
2021-08-04 04:56:35 +0900

    [CHORE] 세팅 스토리보드 수정

---
+++ Awesome/Source/Models/InviteCountModel.swift
@@ -0,0 +1,8 @@
+//
+//  InviteCountModel.swift
+//  Awesome
+//
+//  Created by 박익범 on 2021/08/04.
+//
+
+import Foundation
---
+++ Awesome/Source/Models/KakaoLogoutModel.swift
@@ -0,0 +1,8 @@
+//
+//  KakaoLogoutModel.swift
+//  Awesome
+//
+//  Created by 박익범 on 2021/08/04.
+//
+
+import Foundation
