e6b303c3697de7b819dee121fd5a96ec2f35c1c4
parkikbum <pibum@naver.com>
2021-08-04 04:59:42 +0900

    [ADD]초대장 발송 데이터 서비스

--- Awesome/Source/Services/GetInviteDataService.swift
+++ Awesome/Source/Services/GetInviteDataService.swift
@@ -1,8 +1,59 @@
-//
-//  GetInviteDataService.swift
-//  Awesome
-//
-//  Created by 박익범 on 2021/08/04.
-//
-
+import Alamofire
 import Foundation
+
+
+
+
+struct GetInviteService
+{
+    static let inviteData = GetInviteService()
+    let userToken = UserDefaults.standard.string(forKey: "accessToken") ?? ""
+    
+    func getRecommendInfo(completion : @escaping (NetworkResult<Any>) -> Void)
+    {
+        // completion 클로저를 @escaping closure로 정의합니다.
+        let URL = Constants.inviteURL
+        let header : HTTPHeaders = ["Authorization": "Bearer " + userToken]
+        let dataRequest = AF.request(URL,
+                                     method: .post,
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+        dataRequest.responseData { dataResponse in
+            switch dataResponse.result {
+            case .success:
+                guard let statusCode = dataResponse.response?.statusCode else {return}
+                guard let value = dataResponse.value else {return}
+                print("dd", statusCode)
+                let networkResult = self.judgeStatus(by: statusCode, value)
+                completion(networkResult)
+            
+            case .failure: completion(.pathErr)
+                print("실패 사유")
+
+                
+            }
+        }
+    }
+    private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        switch statusCode {
+        case 200: return isValidData(data: data)
+        case 400: return .pathErr
+        case 500: return .serverErr
+        default: return .networkFail
+        }
+    }
+    
+    private func isValidData(data : Data) -> NetworkResult<Any> {
+        
+        let defaults = UserDefaults.standard
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(InviteDataModel.self, from: data)
+        else {return .pathErr}
+
+        print(decodedData)
+    
+        return .success(decodedData)
+
+    }
+    
+}
