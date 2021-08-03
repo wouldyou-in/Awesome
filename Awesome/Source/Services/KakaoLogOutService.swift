1a9e3e76fff397a03ccfae8427adf93cc9fb75af
parkikbum <pibum@naver.com>
2021-08-04 05:00:11 +0900

    [ADD]카카오 로그아웃 데이터 서비스

--- Awesome/Source/Services/KakaoLogOutService.swift
+++ Awesome/Source/Services/KakaoLogOutService.swift
@@ -1,8 +1,68 @@
-//
-//  KakaoLogOutService.swift
-//  Awesome
-//
-//  Created by 박익범 on 2021/08/04.
-//
-
 import Foundation
+import Alamofire
+
+struct GetLogoutService {
+    static let shared = GetLogoutService()
+    let userToken = UserDefaults.standard.string(forKey: "accessToken") ?? ""
+    
+    private func makeParameter(refresh_token : String) -> Parameters
+    {
+        return ["refresh_token": refresh_token]
+    }
+    
+    func AutoLoginService(refresh_token : String,
+              completion : @escaping (NetworkResult<Any>) -> Void)
+    {
+        let header : HTTPHeaders = ["Authorization": "Bearer " + userToken]
+        let dataRequest = AF.request(Constants.logoutURL,
+                                     method: .post,
+                                     parameters: makeParameter(refresh_token: refresh_token),
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+        
+        
+        dataRequest.responseData { dataResponse in
+            
+            switch dataResponse.result {
+            case .success:
+                
+                print("----- 데이터 요청 성공")
+                print(userToken," aaa ", refresh_token)
+                guard let statusCode = dataResponse.response?.statusCode else {return}
+                print(dataRequest.response?.statusCode)
+                guard let value = dataResponse.value else {return}
+                let networkResult = self.judgeStatus(by: statusCode, value)
+                completion(networkResult)
+                
+            case .failure: completion(.pathErr)
+                
+            }
+        }
+        
+    }
+    
+    private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        
+        let decoder = JSONDecoder()
+        
+        
+        guard let decodedData = try? decoder.decode(LogoutDataModel.self, from: data)
+        else {
+            print("패쓰에러")
+            return .pathErr
+            
+        }
+        
+        switch statusCode {
+        
+        case 200:
+            print("--- 데이터 받기 성공")
+            return .success(decodedData)
+        case 400: return .requestErr(decodedData)
+        case 500: return .serverErr
+        default: return .networkFail
+        }
+    }
+    
+    
+}
