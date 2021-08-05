a51b53f370c6c7718fbbc6500120dd083f498a0a
parkikbum <pibum@naver.com>
2021-08-05 19:11:02 +0900

    [ADD]블락데이터 서비스 구현

--- Awesome/Source/Services/PostBlockDataService.swift
+++ Awesome/Source/Services/PostBlockDataService.swift
@@ -1,8 +1,67 @@
-//
-//  PostBlockDataService.swift
-//  Awesome
-//
-//  Created by 박익범 on 2021/08/05.
-//
-
 import Foundation
+import Alamofire
+
+struct PostBlockDataService {
+    static let shared = PostBlockDataService()
+    let userToken = UserDefaults.standard.string(forKey: "accessToken") ?? ""
+    
+    private func makeParameter(start_date: String, end_date: String) -> Parameters
+    {
+        return ["start_date": start_date, "end_date": end_date]
+    }
+    
+    func postScheduleService(start_date: String, end_date: String,
+              completion : @escaping (NetworkResult<Any>) -> Void)
+    {
+        let header : HTTPHeaders = ["Authorization": "Bearer " + userToken]
+        let dataRequest = AF.request(Constants.blockDateURL,
+                                     method: .post,
+                                     parameters: makeParameter(start_date: start_date, end_date: end_date),
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+        
+        
+        dataRequest.responseData { dataResponse in
+            
+            switch dataResponse.result {
+            case .success:
+                
+                print("-----블락 데이터 요청 성공")
+                guard let statusCode = dataResponse.response?.statusCode else {return}
+                print(dataRequest.response?.statusCode)
+                guard let value = dataResponse.value else {return}
+                let networkResult = self.judgeStatus(by: statusCode, value)
+                completion(networkResult)
+                
+            case .failure: completion(.pathErr)
+                
+            }
+        }
+        
+    }
+    
+    private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        
+        let decoder = JSONDecoder()
+        
+        
+        guard let decodedData = try? decoder.decode(PostBlockDataModel.self, from: data)
+        else {
+            print("패쓰에러")
+            return .pathErr
+            
+        }
+        
+        switch statusCode {
+        
+        case 200:
+            print("--- 블락 데이터 받기 성공")
+            return .success(decodedData)
+        case 400: return .requestErr(decodedData)
+        case 500: return .serverErr
+        default: return .networkFail
+        }
+    }
+    
+    
+}
