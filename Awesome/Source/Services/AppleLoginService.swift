769195bd912fd1916bd13dac468df1df79172caf
parkikbum <pibum@naver.com>
2021-08-05 20:30:16 +0900

    [ADD]애플 로그인서비스 추가

--- Awesome/Source/Services/AppleLoginService.swift
+++ Awesome/Source/Services/AppleLoginService.swift
@@ -1,8 +1,66 @@
-//
-//  AppleLoginService.swift
-//  Awesome
-//
-//  Created by 박익범 on 2021/08/05.
-//
-
 import Foundation
+import Alamofire
+
+struct AppleLoginService {
+    static let shared = AppleLoginService()
+
+    private func makeParameter(code: String) -> Parameters
+    {
+        return ["code": code]
+    }
+    
+    func postScheduleService(code: String,
+              completion : @escaping (NetworkResult<Any>) -> Void)
+    {
+        let header : HTTPHeaders = ["Content-Type": "application/json"]
+        let dataRequest = AF.request(Constants.appleLogin,
+                                     method: .post,
+                                     parameters: makeParameter(code: code),
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+        
+        
+        dataRequest.responseData { dataResponse in
+            
+            switch dataResponse.result {
+            case .success:
+                
+                print("-----애플 데이터 요청 성공")
+                guard let statusCode = dataResponse.response?.statusCode else {return}
+                print(dataRequest.response?.statusCode)
+                guard let value = dataResponse.value else {return}
+                let networkResult = self.judgeStatus(by: statusCode, value)
+                completion(networkResult)
+                
+            case .failure: completion(.pathErr)
+                
+            }
+        }
+        
+    }
+    
+    private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        
+        let decoder = JSONDecoder()
+        
+        
+        guard let decodedData = try? decoder.decode(PostBlockDataModel.self, from: data)
+        else {
+            print("패쓰에러")
+            return .pathErr
+            
+        }
+        
+        switch statusCode {
+        
+        case 200:
+            print("--- 애플 데이터 받기 성공")
+            return .success(decodedData)
+        case 400: return .requestErr(decodedData)
+        case 500: return .serverErr
+        default: return .networkFail
+        }
+    }
+    
+    
+}
