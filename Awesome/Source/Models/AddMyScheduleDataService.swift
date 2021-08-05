d2cb903c4c844678b4406748ec55d19bd1844640
parkikbum <pibum@naver.com>
2021-08-05 17:18:03 +0900

    [ADD] 내 약속 추가하기 서비스

---
+++ Awesome/Source/Models/AddMyScheduleDataService.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+// MARK: - Welcome
+struct PostCalendarDataModel: Codable {
+    let status: String
+    let data: PostCalendar
+}
+
+// MARK: - DataClass
+struct PostCalendar: Codable {
+    let id, creator: Int
+    let creatorName: String
+    let participant: Int
+    let participantName, comment: String
+    let startDate, endDate: Date
+    let createdAt: String
+    let isAccept: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id, creator
+        case creatorName = "creator_name"
+        case participant
+        case participantName = "participant_name"
+        case comment
+        case startDate = "start_date"
+        case endDate = "end_date"
+        case createdAt = "created_at"
+        case isAccept = "is_accept"
+    }
+}
+
---
+++ Awesome/Source/Services/AddMyscheduleDataModel.swift
@@ -0,0 +1,67 @@
+import Foundation
+import Alamofire
+
+struct PostScheduleDataService {
+    static let shared = PostScheduleDataService()
+    let userToken = UserDefaults.standard.string(forKey: "accessToken") ?? ""
+    
+    private func makeParameter(comment : String, start_date: String, end_date: String, receiver_id: Int) -> Parameters
+    {
+        return ["comment": comment, "start_date": start_date, "end_date": end_date, "receiver_id": receiver_id]
+    }
+    
+    func postScheduleService(comment : String, start_date: String, end_date: String, receiver_id: Int,
+              completion : @escaping (NetworkResult<Any>) -> Void)
+    {
+        let header : HTTPHeaders = ["Authorization": "Bearer " + userToken]
+        let dataRequest = AF.request(Constants.postCalendearURL,
+                                     method: .post,
+                                     parameters: makeParameter(comment: comment, start_date: start_date, end_date: end_date, receiver_id: receiver_id),
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+        
+        
+        dataRequest.responseData { dataResponse in
+            
+            switch dataResponse.result {
+            case .success:
+                
+                print("-----내 일정 데이터 요청 성공")
+                guard let statusCode = dataResponse.response?.statusCode else {return}
+                print(dataRequest.response?.statusCode)
+                guard let value = dataResponse.value else {return}
+                let networkResult = self.judgeStatus(by: statusCode, value)
+                completion(networkResult)
+                
+            case .failure: completion(.pathErr)
+                
+            }
+        }
+        
+    }
+    
+    private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        
+        let decoder = JSONDecoder()
+        
+        
+        guard let decodedData = try? decoder.decode(PostCalendarDataModel.self, from: data)
+        else {
+            print("패쓰에러")
+            return .pathErr
+            
+        }
+        
+        switch statusCode {
+        
+        case 200:
+            print("--- 내 일정 올리는 데이터 받기 성공")
+            return .success(decodedData)
+        case 400: return .requestErr(decodedData)
+        case 500: return .serverErr
+        default: return .networkFail
+        }
+    }
+    
+    
+}
