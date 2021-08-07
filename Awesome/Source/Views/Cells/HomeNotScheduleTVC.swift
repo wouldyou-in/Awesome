768c83205b18177d570e195bb330d362b0716758
parkikbum <pibum@naver.com>
2021-08-07 14:18:20 +0900

    [ADD]홈 pull down시 제스처 기능

--- Awesome/Source/Views/Cells/HomeNotScheduleTVC.swift
+++ Awesome/Source/Views/Cells/HomeNotScheduleTVC.swift
@@ -9,13 +9,21 @@ import UIKit
 
 class HomeNotScheduleTVC: UITableViewCell {
 
+    static let identifier: String = "HomeNotScheduleTVC"
+
     override func awakeFromNib() {
         super.awakeFromNib()
+        setBackground()
         // Initialization code
+    }
+    
+    func setBackground(){
+        self.backgroundColor = UIColor.mainGray
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+        selectionStyle = .none
 
         // Configure the view for the selected state
     }
