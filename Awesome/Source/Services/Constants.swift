97c5850990558946de4b3935855ca77581b25029
parkikbum <pibum@naver.com>
2021-08-08 20:21:02 +0900

    URL수정

--- Awesome/Source/Services/Constants.swift
+++ Awesome/Source/Services/Constants.swift
@@ -8,13 +8,12 @@ struct Constants {
     static let baseURL = "https://api.wouldyou.in"
     
     // MARK: - Feature URL
-    
+    static var loginString = "https://api.wouldyou.in/user/kakao/callback/"
     /// 1.로그인 리스트 URL
     static var LoginURL = baseURL + "/user/kakao/login/"
     static var updateLoginToken = baseURL + "/user/refresh/"
     static var logoutURL = baseURL + "/user/logout/"
     static var appleLogin = baseURL + "/user/apple/login/"
-    
     /// 2.프로필 리스트 URL
     static var profileDataURL = baseURL + "/user/me/"
     /// 3.약속 리스트 URL
