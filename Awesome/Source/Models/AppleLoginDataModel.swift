72055c500e386db97e96fde51e6283700a946a04
parkikbum <pibum@naver.com>
2021-08-11 00:31:22 +0900

    [ADD]애플로그인 서비스

--- Awesome/Source/Models/AppleLoginDataModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AppleLoginDataModel.swift
-//  Awesome
-//
-//  Created by 박익범 on 2021/08/05.
-//
-
-import Foundation
--- Awesome/Source/Services/AppleLoginService.swift
+++ Awesome/Source/Services/AppleLoginService.swift
@@ -4,18 +4,18 @@ import Alamofire
 struct AppleLoginService {
     static let shared = AppleLoginService()
 
-    private func makeParameter(code: String) -> Parameters
+    private func makeParameter(code: String, name: String) -> Parameters
     {
-        return ["code": code]
+        return ["code": code, "name": name]
     }
     
-    func postScheduleService(code: String,
+    func postScheduleService(code: String, name: String,
               completion : @escaping (NetworkResult<Any>) -> Void)
     {
         let header : HTTPHeaders = ["Content-Type": "application/json"]
         let dataRequest = AF.request(Constants.appleLogin,
                                      method: .post,
-                                     parameters: makeParameter(code: code),
+                                     parameters: makeParameter(code: code, name: name),
                                      encoding: JSONEncoding.default,
                                      headers: header)
         
@@ -44,7 +44,7 @@ struct AppleLoginService {
         let decoder = JSONDecoder()
         
         
-        guard let decodedData = try? decoder.decode(PostBlockDataModel.self, from: data)
+        guard let decodedData = try? decoder.decode(KakaoLoginTokenModel.self, from: data)
         else {
             print("패쓰에러")
             return .pathErr
@@ -55,6 +55,13 @@ struct AppleLoginService {
         
         case 200:
             print("--- 애플 데이터 받기 성공")
+            let defaults = UserDefaults.standard
+            defaults.set(decodedData.refreshToken, forKey: "refreshToken")
+            defaults.set(decodedData.accessToken , forKey: "accessToken")
+            defaults.set(decodedData.user.name, forKey: "myName")
+            defaults.set(decodedData.user.id, forKey: "myKey")
+            defaults.set(true, forKey: "appleLoginSuccess")
+            defaults.set(true, forKey: "loginBool")
             return .success(decodedData)
         case 400: return .requestErr(decodedData)
         case 500: return .serverErr
