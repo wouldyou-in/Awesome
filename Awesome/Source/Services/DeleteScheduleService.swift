3637ca5109231f56d75d83058d013b85654c53a6
parkikbum <pibum@naver.com>
2021-08-17 15:48:59 +0900

    [ADD]삭제 서비스 파일 추가

--- Awesome/Source/Services/DeleteScheduleService.swift
+++ Awesome/Source/Services/DeleteScheduleService.swift
@@ -1,8 +1,66 @@
-//
-//  DeleteScheduleService.swift
-//  Awesome
-//
-//  Created by 박익범 on 2021/08/17.
-//
-
 import Foundation
+import Alamofire
+
+struct DeleteScheduleService {
+    static let shared = DeleteScheduleService()
+    let userToken = UserDefaults.standard.string(forKey: "accessToken") ?? ""
+    
+    private func makeParameter(id : Int, comment: String, participant: Int) -> Parameters
+    {
+        return ["id": id , "comment": comment, "participant": participant]
+    }
+    
+    func DeleteService(id : Int, comment: String, participant: Int,
+              completion : @escaping (NetworkResult<Any>) -> Void)
+    {
+        let header : HTTPHeaders = ["Authorization": "Bearer " + userToken]
+        let dataRequest = AF.request(Constants.postCalendearURL,
+                                     method: .delete,
+                                     parameters: makeParameter(id: id, comment: comment, participant: participant),
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+        
+        dataRequest.responseData { dataResponse in
+            
+            switch dataResponse.result {
+            case .success:
+                
+                print("-----약속 삭제 업데이트 데이터 요청 성공")
+                guard let statusCode = dataResponse.response?.statusCode else {return}
+                print(dataRequest.response?.statusCode)
+                guard let value = dataResponse.value else {return}
+                let networkResult = self.judgeStatus(by: statusCode, value)
+                completion(networkResult)
+                
+            case .failure: completion(.pathErr)
+                
+            }
+        }
+        
+    }
+    
+    private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        
+        let decoder = JSONDecoder()
+        
+        
+        guard let decodedData = try? decoder.decode(PostTokenDataModel.self, from: data)
+        else {
+            print("패쓰에러")
+            return .pathErr
+            
+        }
+        
+        switch statusCode {
+        
+        case 200:
+            print("---약속 삭제 데이터 받기 성공")
+            return .success(decodedData)
+        case 400: return .requestErr(decodedData)
+        case 500: return .serverErr
+        default: return .networkFail
+        }
+    }
+    
+    
+}
