62b88e074825ffa0f3e53b492652580edaa2ff1a
parkikbum <pibum@naver.com>
2021-08-19 21:42:31 +0900

    [ADD]블락데이터 삭제 서비스

--- Awesome/Source/Services/DeleteBlockSchedule.swift
+++ Awesome/Source/Services/DeleteBlockSchedule.swift
@@ -1,8 +1,66 @@
-//
-//  DeleteBlockSchedule.swift
-//  Awesome
-//
-//  Created by 박익범 on 2021/08/18.
-//
-
 import Foundation
+import Alamofire
+
+struct DeleteBlockScheduleService {
+    static let shared = DeleteBlockScheduleService()
+    let userToken = UserDefaults.standard.string(forKey: "accessToken") ?? ""
+    
+    private func makeParameter(id : Int) -> Parameters
+    {
+        return ["id": id]
+    }
+    
+    func DeleteService(id : Int,
+              completion : @escaping (NetworkResult<Any>) -> Void)
+    {
+        let header : HTTPHeaders = ["Authorization": "Bearer " + userToken]
+        let dataRequest = AF.request(Constants.blockDateURL,
+                                     method: .delete,
+                                     parameters: makeParameter(id: id),
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+        
+        dataRequest.responseData { dataResponse in
+            
+            switch dataResponse.result {
+            case .success:
+                
+                print("-----약속 안받는 기간 업데이트 데이터 요청 성공")
+                guard let statusCode = dataResponse.response?.statusCode else {return}
+                print(dataRequest.response?.statusCode)
+                guard let value = dataResponse.value else {return}
+                let networkResult = self.judgeStatus(by: statusCode, value)
+                completion(networkResult)
+                
+            case .failure: completion(.pathErr)
+                
+            }
+        }
+        
+    }
+    
+    private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        
+        let decoder = JSONDecoder()
+        
+        
+        guard let decodedData = try? decoder.decode(PostTokenDataModel.self, from: data)
+        else {
+            print("패쓰에러")
+            return .pathErr
+            
+        }
+        
+        switch statusCode {
+        
+        case 200:
+            print("---약속 안받는 기간 데이터 받기 성공")
+            return .success(decodedData)
+        case 400: return .requestErr(decodedData)
+        case 500: return .serverErr
+        default: return .networkFail
+        }
+    }
+    
+    
+}
