4ee29c9225ca1354570daa1b7174bc742e98c090
parkikbum <pibum@naver.com>
2021-09-13 02:16:14 +0900

    [CHORE]데이터 모델 변경

--- Awesome/Source/Models/ScheduleNotice.swift
+++ Awesome/Source/Models/ScheduleNotice.swift
@@ -1,15 +1,5 @@
 import Foundation
 
-public protocol DefaultCodableStrategy {
-    associatedtype DefaultValue: Decodable
-    
-    /// The fallback value used when decoding fails
-    static var defaultValue: DefaultValue { get }
-}
-
-public struct DefaultFalseStrategy: DefaultCodableStrategy {
-    public static var defaultValue: Bool { return false }
-}
 
 // MARK: - ScheduleNoticeModel
 struct ScheduleNoticeModel: Codable {
@@ -21,18 +11,16 @@ struct ScheduleNoticeModel: Codable {
 
 // MARK: - ScheduleNoticeDataModel
 struct ScheduleNoticeDataModel: Codable {
-    let id, creator: Int
-       let creatorName: String
+       let id: Int
+       let creator: Int?
        let participant: Int
        let participantName, comment: String
-       let startDate, endDate: Date
+       let startDate, endDate: String
        let createdAt: String
        let isAccept: Bool?
 
        enum CodingKeys: String, CodingKey {
-           case id, creator
-           case creatorName = "creator_name"
-           case participant
+           case id, creator, participant
            case participantName = "participant_name"
            case comment
            case startDate = "start_date"
@@ -41,18 +29,5 @@ struct ScheduleNoticeDataModel: Codable {
            case isAccept = "is_accept"
        }
 
-//    init(from decoder : Decoder) throws
-//        {
-//        let values = try decoder.container(keyedBy: CodingKeys.self)
-//        id = (try? values.decode(Int.self, forKey: .id)) ?? 0
-//        creator = (try? values.decode(Int.self, forKey: .creator)) ?? 0
-//        creatorName = (try? values.decode(String.self, forKey: .creatorName)) ?? ""
-//        participant = (try? values.decode(Int.self, forKey: .participant)) ?? 0
-//        participantName = (try? values.decode(String.self, forKey: .participantName)) ?? ""
-//        comment = (try? values.decode(String.self, forKey: .comment)) ?? ""
-//        startDate = (try? values.decode(Date.self, forKey: .startDate)) ?? Date()
-//        endDate = (try? values.decode(Date.self, forKey: .endDate)) ?? Date()
-//        createdAt = (try? values.decode(String.self, forKey: .createdAt)) ?? ""
-//        isAccept = (try? values.decode(Bool.self, forKey: .isAccept)) ?? false
-//        }
+
 }
